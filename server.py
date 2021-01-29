2747d0b7bc7f3c9efe0f8806b7e0f08668db555d
yanshan24 <>
2021-01-29 14:28:13 +0800

    add license

--- server.py
+++ server.py
@@ -20,6 +20,21 @@ import socketserver, os
 # some of the code is Copyright © 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
+#
+# Copyright 2021 Yanshan Wan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # http://docs.python.org/2/library/socketserver.html
 #
 # run: python freetests.py
@@ -52,7 +67,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
                 address = self.server.server_address
                 server_url = "http://" + address[0] + ":" + str(address[1])
                 location = server_url + url + "/" # http://127.0.0.1:8080/.../
-                self.request.sendall(bytearray(f"HTTP/1.1 301 Moved Permanently\r\nLocation:{location}\r\n\r\n301 Moved Permanently",'utf-8'))
+                self.request.sendall(bytearray(f"HTTP/1.1 301 Moved Permanently\r\nLocation: {location}\r\n\r\n301 Moved Permanently",'utf-8'))
                 return
             else:
                 self.sendOK(local_path, url, is_Dir = True)
